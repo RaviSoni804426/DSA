82980d6ac7826535ade5a993ae6b69ec3c371333
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-05-11 12:42:48 +0530

    Add files via upload

---
+++ STRIVER/L1.cpp
@@ -0,0 +1,64 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+    // int x=10;
+    // long x = 15;
+    // string s1;
+    // string s2;
+    // cin>>s1>>s2;
+    // cout<<s1<<" "<<s2;
+    // string str;
+    // getline(cin,str);
+    // cout<<str;
+    // char ch;
+    // cin>>ch;
+    // cout<<ch;
+    // int age;
+    // cin>>age;
+    // if (age>=18){
+    // cout<<"You are old";
+    // }
+    // else{
+    // cout<<"you are young";
+    // }
+    // int day;
+    // cin>>day;
+    // switch(day){
+    // case 1:
+    //     cout<<"Monday";
+    //     break;
+    // case 2:
+    //     cout<<"februry";
+    //     break;
+    // default:
+    // cout<<"invalid";
+// int arr[3][5];
+// cin>>arr[0]>>
+// cin>>a>>b>>c>>d;
+// cout<<a<<' '<<b<<c;
+//  return 0;
+// }
+// int 
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// void printname(string name){
+//     cout<<"Hello "<<name<<endl;
+// }
+// int main(){
+//     string name;
+//     cin>>name;
+//     printname(name);
+// }
+// int sum(int num1,int num2){
+    // int num3=num1+num2;
+    // return num3;
+// }
+// int main(){
+// int res;
+// =sum(2,4);
+// cout<<res;
+// return 0;
+// }
+
---
+++ STRIVER/L3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,j,n;
+    cin>>n;
+for (i=1;i<=n;i++){
+    //space
+    for
+    for (j=1;j<=(n-i+1);j++){
+        cout<< j<< " ";
+    }
+cout<<endl;
+
+}
+}
